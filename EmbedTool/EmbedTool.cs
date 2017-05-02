992c0c3364226097f8e5b1bfc878d62b92ea61d9
MCUnderground <daniel.barinak@gmail.com>
2017-05-03 00:07:20 +0200

    All

---
+++ EmbedTool/EmbedTool.cs
@@ -0,0 +1,65 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmbedsOnFly
+{
+    public class EmbedTool : ModuleBase
+    {
+        EmbedBuilder basic;
+        EmbedFooterBuilder footer;
+
+        public void MakeEmbed(string color = "white", string title = null, string description = null, string imageUrl = null, string footerDescription = null, string footerImageUrl = null)
+        {
+            EmbedColor(color);
+            if (title != null) { basic.WithTitle(title); }
+            if (description != null) { basic.WithDescription(description); }
+            if (imageUrl != null) { basic.WithThumbnailUrl(imageUrl); }
+
+            footer = new EmbedFooterBuilder();
+
+            if (footerDescription != null)
+            {
+                basic.WithFooter(footer
+                     .WithText(footerDescription)
+                );
+            }
+            if (footerImageUrl != null)
+            {
+                basic.WithFooter(footer
+                     .WithIconUrl(footerImageUrl)
+                );
+            }
+        }
+        private void EmbedColor(string color)
+        {
+            basic = new EmbedBuilder();
+
+
+            if (color.ToLower() == "aqua") { basic.WithColor(new Color(0, 255, 255)); }
+            else if (color.ToLower() == "black") { basic.WithColor(new Color(0, 0, 0)); }
+            else if (color.ToLower() == "blue") { basic.WithColor(new Color(0, 0, 255)); }
+            else if (color.ToLower() == "fuchsia") { basic.WithColor(new Color(255, 0, 255)); }
+            else if (color.ToLower() == "gray") { basic.WithColor(new Color(128, 128, 128)); }
+            else if (color.ToLower() == "green") { basic.WithColor(new Color(0, 128, 0)); }
+            else if (color.ToLower() == "lime") { basic.WithColor(new Color(0, 255, 0)); }
+            else if (color.ToLower() == "maroon") { basic.WithColor(new Color(128, 0, 0)); }
+            else if (color.ToLower() == "navy") { basic.WithColor(new Color(0, 0, 128)); }
+            else if (color.ToLower() == "olive") { basic.WithColor(new Color(128, 128, 0)); }
+            else if (color.ToLower() == "purple") { basic.WithColor(new Color(128, 0, 128)); }
+            else if (color.ToLower() == "red") { basic.WithColor(new Color(255, 0, 0)); }
+            else if (color.ToLower() == "silver") { basic.WithColor(new Color(192, 192, 192)); }
+            else if (color.ToLower() == "teal") { basic.WithColor(new Color(0, 128, 128)); }
+            else if (color.ToLower() == "white") { basic.WithColor(new Color(255, 255, 255)); }
+            else if (color.ToLower() == "yellow") { basic.WithColor(new Color(255, 255, 0)); }
+            else { basic.WithColor(new Color(255, 255, 255)); }
+
+        }
+
+        public async Task SendEmbed(CommandContext Context) { await Context.Channel.SendMessageAsync("", false, basic); }
+    }
+}
---
+++ EmbedTool/README.md
@@ -0,0 +1,29 @@
+# EmbedTool
+Faster embeds for Discord.Net 1.0+ warapper
+
+Install: 
+```tex
+Install-Package Discord.Addons.EmbedTool
+```
+
+Example Usage:
+
+```cpp
+EmbedTool embed = new EmbedTool();
+
+embed.MakeEmbed("red", "Example", "this is example");
+embed.SendEmbed(Context);
+```
+
+Colors supported :
+
+Colors | to | use <3
+--- | --- | ---
+Aqua | Black | Blue
+Fuchsia | Gray | Green
+Lime | Maroon | Navy
+Olive | Purple | Red
+Silver | Teal | White
+Yellow | / | /
+
+Colors followed from: https://en.wikipedia.org/wiki/Web_colors#HTML_color_names
